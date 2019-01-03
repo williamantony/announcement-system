d7a0d5681324df8325ee08fc1af76d391482bacd
William Antony <william@willsquad.com>
2019-01-02 23:37:01 -0600

    Created CreateEventForm Component

--- src/components/CreateEventForm/CreateEventForm.js
+++ src/components/CreateEventForm/CreateEventForm.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './CreateEventForm.css';
-import EventTitleInput from './components/EventTitleInput/EventTitleInput';
 
 class CreateEventForm extends Component {
   constructor(props) {
@@ -10,10 +9,37 @@ class CreateEventForm extends Component {
     };
   }
 
+  handleInput = (event, name) => {
+    const { value } = event.target;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
     return (
       <div className="CreateEventForm">
-        <EventTitleInput />
+        <div className="EventCard">
+          <div className="EventCard__title">
+            <input
+              type="text"
+              name="title"
+              value={this.state.title}
+              placeholder="Name of the Event"
+              className="EventCard__title__input"
+              onChange={(e) => this.handleInput(e, 'title')}
+            />
+          </div>
+          
+          <div className="EventCard__action">
+            <div className="EventCard__action__button">
+              <div className="text">Add Date & Time</div>
+              <div className="icon" />
+            </div>
+          </div>
+
+        </div>
       </div>
     );
   }
