c351040ffcf67626317a5325028952919b05c4d5
William Antony <william@willsquad.com>
2019-01-04 22:30:01 -0600

    Modified CreateEventForm for prospective layouts

--- src/components/CreateEventForm/CreateEventForm.js
+++ src/components/CreateEventForm/CreateEventForm.js
@@ -20,9 +20,11 @@ class CreateEventForm extends Component {
   render() {
     return (
       <div className="CreateEventForm">
+
         <div className="EventCard">
           <div className="EventCard__title">
-            <input
+            <div className="EventCard__title__label">Create Event</div>
+            <textarea
               type="text"
               name="title"
               value={this.state.title}
@@ -31,15 +33,122 @@ class CreateEventForm extends Component {
               onChange={(e) => this.handleInput(e, 'title')}
             />
           </div>
-          
           <div className="EventCard__action">
-            <div className="EventCard__action__button">
-              <div className="text">Add Date & Time</div>
+            <div className="EventCard__action__button EventCard__action__button--clear">
+              <div className="text">Clear</div>
+              <div className="icon" />
+            </div>
+            <div className="EventCard__action__button EventCard__action__button--save">
+              <div className="text">Continue</div>
               <div className="icon" />
             </div>
           </div>
-
         </div>
+
+        <div className="EventCard">
+          <div className="EventCard__title">
+            <div className="EventCard__title__label">Choose Type</div>
+            <div>
+              <input type="radio" />
+              One Day
+            </div>
+            <div>
+              <input type="radio" />
+              Multiple days in a row
+            </div>
+            <div>
+              <input type="radio" />
+              Multiple days, but NOT in a row
+            </div>
+
+            <hr/>
+
+            <div>
+              <input type="radio" />
+              Single Session
+            </div>
+            <div>
+              <input type="radio" />
+              Multiple Sessions
+            </div>
+            <div>
+              <input type="radio" />
+              Custom
+            </div>
+          </div>
+          <div className="EventCard__action">
+            <div className="EventCard__action__button EventCard__action__button--clear">
+              <div className="text">Clear</div>
+              <div className="icon" />
+            </div>
+            <div className="EventCard__action__button EventCard__action__button--save">
+              <div className="text">Continue</div>
+              <div className="icon" />
+            </div>
+          </div>
+        </div>
+
+        <div className="EventCard">
+          <div className="EventCard__title">
+            <div className="EventCard__title__label">Session Name</div>
+            <textarea
+              type="text"
+              name="title"
+              value={this.state.title}
+              placeholder="Name of the Session (optional)"
+              className="EventCard__title__input"
+              onChange={(e) => this.handleInput(e, 'title')}
+            />
+          </div>
+
+          <div className="EventCard__title">
+            <div className="EventCard__title__label">Date</div>
+            <input
+              type="date"
+              name="title"
+              value={this.state.title}
+              placeholder="Name of the Session (optional)"
+              className="EventCard__title__input"
+              onChange={(e) => this.handleInput(e, 'title')}
+            />
+          </div>
+
+          <div className="EventCard__title">
+            <div className="EventCard__title__label">Time</div>
+            <input
+              type="time"
+              name="title"
+              value={this.state.title}
+              placeholder="Name of the Session (optional)"
+              className="EventCard__title__input"
+              onChange={(e) => this.handleInput(e, 'title')}
+            />
+          </div>
+          
+          <div className="EventCard__title">
+            <div className="EventCard__title__label">Note</div>
+            <textarea
+              type="text"
+              name="title"
+              value={this.state.title}
+              placeholder="Message to the attendees (optional)"
+              className="EventCard__title__input"
+              onChange={(e) => this.handleInput(e, 'title')}
+            />
+          </div>
+          
+          <div className="EventCard__action">
+            <div className="EventCard__action__button EventCard__action__button--clear">
+              <div className="text">Clear</div>
+              <div className="icon" />
+            </div>
+            <div className="EventCard__action__button EventCard__action__button--save">
+              <div className="text">Continue</div>
+              <div className="icon" />
+            </div>
+          </div>
+        </div>
+
       </div>
     );
   }
