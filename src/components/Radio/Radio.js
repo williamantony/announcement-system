02684945069dc8f08ffb3bd247964fdd8bb11242
William Antony <william@willsquad.com>
2019-01-05 23:19:03 -0600

    Created Radio Component

---
+++ src/components/Radio/Radio.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import uuid from 'uuid/v4';
+import './Radio.css';
+
+class Radio extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: props.value || '',
+      options: props.options || [],
+    };
+  }
+
+  handleValueChange = (value) => {
+    this.setState({ value });
+  }
+
+  render() {
+    return (
+      <div className="Radio">
+        {
+          this.state.options.map(item => {
+            const selectedClass = (this.state.value === item.value) ? `RadioItem--selected` : ``;
+
+            return (
+              <div
+                className={`RadioItem ${ selectedClass }`}
+                onClick={(e) => this.handleValueChange(item.value)}
+              >
+                <div className="RadioItem__button">
+                  <div className="RadioItem__button__circle" />
+                </div>
+                <div className="RadioItem__label">
+                  <div className="RadioItem__label__text">
+                    { item.label }
+                  </div>
+                </div>
+              </div>
+            );
+          })
+        }
+      </div>
+    );
+  }
+
+}
+
+export default Radio;
