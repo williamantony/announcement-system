22a39bd3b145d601cb2d5057f9a2d7e585917e7d
William Antony <william@willsquad.com>
2019-01-05 23:18:53 -0600

    Created Input Component

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,30 +1,99 @@
 import React, { Component } from 'react';
+import uuid from 'uuid/v4';
 import './Input.css';
 
 class Input extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      id: uuid(),
+      type: props.type || '',
+      name: props.name || '',
+      label: props.label || '',
+      placeholder: props.placeholder || '',
+      value: props.value || '',
+      otherProps: props.otherProps || {},
+      isFocused: false,
+      refs: {
+        input: React.createRef(),
+      },
     };
   }
 
+  handleInput = (event) => {
+    const { value } = event.target;
+    this.setState({ value });
+  }
+
+  handleFocus = (event) => {
+    this.setState({
+      isFocused: true,
+    });
+  }
+
+  handleBlur = (event) => {
+    this.setState({
+      isFocused: false,
+    });
+  }
+
+  handleResize = (event) => {
+    const { target } = event;
+    target.style.height = `${target.scrollHeight - 25}px`;
+    target.style.resize = 'vertical';
+  }
+
+  handleTabFocus = (event) => {
+    this.state.refs.input.current.focus();
+  }
+
   renderInput = () => {
+    const {
+      id,
+      type,
+      name,
+      placeholder,
+      value,
+      otherProps,
+      refs,
+    } = this.state;
+
     const allowedTypes = [
       'text',
+      'date',
+      'time',
       'number',
       'tel',
       'email',
       'search',
     ];
+
+    const commonProps = {
+      ref: refs.input,
+      id: id,
+      className: `Input__input Input__input--${ type }`,
+      type,
+      name,
+      value,
+      placeholder,
+      onChange: this.handleInput,
+      onFocus: this.handleFocus,
+      onBlur: this.handleBlur,
+      ...otherProps,
+    };
     
-    if (allowedTypes.includes(this.props.type)) {
+    if (type === 'textarea') {
       return (
-        <input
-          type={this.props.type}
-          className="Input__input"
-          placeholder={this.props.placeholder}
+        <textarea
+          {...commonProps}
+          onKeyPress={this.handleResize}
         />
+      );
+    }
+    
+    if (allowedTypes.includes(type)) {
+      return (
+        <input {...commonProps} />
       );
     }
 
@@ -32,9 +101,18 @@ class Input extends Component {
   }
 
   render() {
+    const isInputFocused = (this.state.isFocused) ? 'Input__line__line--focused' : '';
+    const lineClassName = `Input__line__line ${ isInputFocused }`;
+
     return (
-      <div className="Input">
-        { this.renderInput() }
+      <div className="Input" tabIndex="0" onFocus={this.handleTabFocus}>
+        <label className="Input__holder" htmlFor={this.state.id}>
+          <div className="Input__label">{ this.state.label }</div>
+          { this.renderInput() }
+          <div className="Input__line">
+            <div className={ lineClassName } />
+          </div>
+        </label>
       </div>
     );
   }
