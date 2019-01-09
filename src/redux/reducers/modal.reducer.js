022147c77fcf4fdce7f47a42b2747c828091491f
William Antony <william@willsquad.com>
2019-01-08 23:53:47 -0600

    Created redux reducer for Modal

---
+++ src/redux/reducers/modal.reducer.js
@@ -0,0 +1,59 @@
+import {
+  CREATE_MODAL,
+  SHOW_MODAL,
+  HIDE_MODAL,
+  DESTROY_MODAL,
+} from '../actions';
+
+const initialState = {
+  instances: {},
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case CREATE_MODAL:
+      return {
+        instances: {
+          ...state.instances,
+          [action.payload.modalId]: {
+            id: action.payload.modalId,
+            content: action.payload.content,
+            isVisible: false,
+          },
+        },
+      };
+
+    case SHOW_MODAL:
+      return {
+        instances: {
+          ...state.instances,
+          [action.payload.modalId]: {
+            isVisible: true,
+          },
+        }
+      };
+
+    case HIDE_MODAL:
+      return {
+        instances: {
+          ...state.instances,
+          [action.payload.modalId]: {
+            isVisible: false,
+          },
+        }
+      };
+
+    case DESTROY_MODAL:
+      return {
+        instances: Object.keys(state.instances).reduce((instances, instanceId) => {
+          if (instanceId !== action.payload.modalId) {
+            instances[instanceId] = state.instances[instanceId];
+          }
+          return instances;
+        }, {}),
+      };
+
+    default:
+      return state;
+  }
+};
