04bc10b8800602cbb2e54356b7d68d3aa47f1f3e
William Antony <william@willsquad.com>
2019-01-08 23:54:21 -0600

    Created acion_creators for Modal

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -36,3 +36,50 @@ export const saveEvent = (eventId) => {
     },
   };
 };
+
+
+export const CREATE_MODAL = 'CREATE_MODAL';
+export const SHOW_MODAL = 'SHOW_MODAL';
+export const HIDE_MODAL = 'HIDE_MODAL';
+export const DESTROY_MODAL = 'DESTROY_MODAL';
+
+export const showModal = (modalId, content = null) => {
+  return dispatch => {
+    dispatch({
+      type: CREATE_MODAL,
+      payload: {
+        modalId,
+        content,
+      },
+    });
+
+    setTimeout(() => {
+      dispatch({
+        type: SHOW_MODAL,
+        payload: {
+          modalId,
+        },
+      });
+    }, 0);
+  };
+};
+
+export const hideModal = (modalId) => {
+  return dispatch => {
+    dispatch({
+      type: HIDE_MODAL,
+      payload: {
+        modalId,
+      },
+    });
+
+    setTimeout(() => {
+      dispatch({
+        type: DESTROY_MODAL,
+        payload: {
+          modalId,
+        },
+      });
+    }, 500);
+  };
+};
