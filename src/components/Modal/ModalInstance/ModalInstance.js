c6f4cae6deb4b9c32dec92f33b3bbf6f9deed4d9
William Antony <william@willsquad.com>
2019-01-09 23:17:04 -0600

    Modified ModalInstance component

--- src/components/Modal/ModalInstance/ModalInstance.js
+++ src/components/Modal/ModalInstance/ModalInstance.js
@@ -7,12 +7,13 @@ import './ModalInstance.css';
 class ModalInstance extends Component {
   constructor(props) {
     super(props);
-    const instance = props.instance;
+    const { instance } = props;
     this.state = {
       id: instance.id || uuid(),
       isVisible: instance.isVisible || false,
-      title: null,
+      title: instance.title || null,
       content: instance.content || null,
+      align: instance.align || 'center',
     };
   }
 
@@ -29,21 +30,23 @@ class ModalInstance extends Component {
 
   render() {
     return (
-      <div className="ModalInstance" data-visible={this.state.isVisible}>
+      <div
+        className="ModalInstance"
+        data-visible={this.state.isVisible}
+        data-align={this.state.align}
+      >
         <div className="ModalInstance__bg" onClick={this.hideModal} />
-        <div className="ModalInstance__box">
-          <div className="ModalInstance__header">
-            <div className="ModalInstance__header__title">
-              { this.state.title }
-            </div>
-            <div className="ModalInstance__header__close" onClick={this.hideModal} >
-              <div className="line" />
-              <div className="line" />
-            </div>
+        <div className="ModalInstance__header">
+          <div className="ModalInstance__header__title">
+            { this.state.title }
           </div>
-          <div className="ModalInstance__box__content">
-            { this.state.content }
+          <div className="ModalInstance__header__close" onClick={this.hideModal} >
+            <div className="line" />
+            <div className="line" />
           </div>
+        </div>
+        <div className="ModalInstance__content">
+          { this.state.content }
         </div>
       </div>
     );
