a01f8762bb14c32433fa3e33df3703bb2f0038b9
William Antony <william@willsquad.com>
2019-01-25 14:48:33 -0600

    Created userVerifyToken() action_creator

--- src/redux/actions/user.action.js
+++ src/redux/actions/user.action.js
@@ -7,6 +7,7 @@ import { createNotification } from './notifications.action';
 export const USER_REGISTER_EMAIL = 'USER_REGISTER_EMAIL';
 export const USER_ACCOUNT_LOGIN = 'USER_ACCOUNT_LOGIN';
 export const USER_SET_PASSWORD = 'USER_SET_PASSWORD';
+export const USER_VERIFICATION = 'USER_VERIFICATION';
 
 export const userRegisterEmail = (email, firstname, lastname, history) => {
   return async dispatch => {
@@ -18,6 +19,7 @@ export const userRegisterEmail = (email, firstname, lastname, history) => {
       });
 
       setTimeout(() => {
+        console.log(response.data);
         if (response.data.error !== null) {
           dispatch(
             createNotification(
@@ -65,6 +67,85 @@ export const userLogin = (username, password) => {
   };
 };
 
-export const userSetPassword = () => {
+export const userVerification = (isVerified, user_id) => {
+  return {
+    type: USER_VERIFICATION,
+    payload: {
+      isVerified,
+      user_id,
+    },
+  };
+};
 
+export const userVerifyToken = (token, history) => {
+  return async dispatch => {
+    try {
+      const response = await axios.post('http://10.0.0.50:5000/user/verify/', {
+        token,
+      });
+
+      console.log(response.data.data);
+
+      if (response.data.error !== null) {
+
+        dispatch(
+          userVerification(false)
+        );
+
+        dispatch(
+          createNotification(
+            'error',
+            'Verification failed - invalid/expired token',
+            'account_verify_email',
+          )
+        );
+
+      } else {
+
+        dispatch(
+          createNotification(
+            'success',
+            'Email address verified',
+            'account_verify_email',
+          )
+        );
+
+        if (response.data.data.hasPassword === false) {
+
+          document.cookie = `token=${ token };path=/password;max-age=${ 60 * 1 };`;
+
+          dispatch(
+            userVerification(true)
+          );
+
+          setTimeout(() => {
+            history.push('/password', { token });
+          }, 1000);
+
+        } else {
+
+          document.cookie = `token=${ token };max-age=${ 60 * 30 };`;
+
+          dispatch(
+            userVerification(true)
+          );
+
+        }
+      }
+    } catch (error) {
+
+      dispatch(
+        createNotification(
+          'error',
+          'Unknown error - please check back later.',
+          'account_verify_email',
+        )
+      );
+
+    }
+  };
+};
+
+export const userSetPassword = (password, token, history) => {
+  
 };
