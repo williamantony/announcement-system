b137375f72c16e60ec4fddbf00fb108f830d4fc3
William Antony <william@willsquad.com>
2019-01-29 23:36:45 -0600

    Created VerifyEmail page Component

---
+++ src/pages/Account/VerifyEmail/VerifyEmail.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { userVerifyToken } from '../../../redux/actions';
+import './VerifyEmail.css';
+import Notifications from '../../../components/Notifications/Notifications';
+import { setCookie } from '../../../helper';
+
+class VerifyEmail extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      token: props.match.params.token,
+    };
+  }
+
+  async componentDidMount() {
+    await setCookie('token', this.state.token, 60 * 1);
+    this.props.userVerifyToken(this.props.history);
+  }
+
+  render() {
+    return (
+      <div className="VerifyEmail">
+        <Notifications group="account_verify_email" />
+      </div>
+    );
+  }
+
+}
+
+const mapStateToProps = state => {
+  return state;
+};
+
+const mapDispatchToProps = {
+  userVerifyToken,
+};
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(VerifyEmail));
