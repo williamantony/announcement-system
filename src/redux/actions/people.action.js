a4bb46f1ea52de4b70cbf182c4820a1f035e863b
William Antony <william@willsquad.com>
2019-02-19 21:27:25 -0600

    Created getPersonInfo() action_creator

--- src/redux/actions/people.action.js
+++ src/redux/actions/people.action.js
@@ -5,16 +5,17 @@ import { getCookie } from '../../helper';
 export const ADD_PERSON = 'ADD_PERSON';
 export const PEOPLE_SET_INFO = 'PEOPLE_SET_INFO';
 
-export const setPersonInfo = (info) => {
+export const setPersonInfo = (person_id, info) => {
   return {
     type: PEOPLE_SET_INFO,
     payload: {
+      person_id,
       info,
     },
   };
 };
 
-export const addPerson = (name = {}, next) => {
+export const getPersonInfo = (person_id = null, returns = []) => {
   return async dispatch => {
     try {
       const token = await getCookie('token');
@@ -22,13 +23,52 @@ export const addPerson = (name = {}, next) => {
       if (!token) {
         throw String('Missing Token');
       }
-      
+
+      const endpoint = `http://10.0.0.50:5000/people/${person_id}`;
+
+      const response = await axios.get(endpoint, {
+        params: {
+          token,
+          returns,
+        },
+      });
+
+      if (response.data.error !== null) {
+        throw String('Failed to get person details');
+      }
+
       dispatch(
-        setPersonInfo(name)
+        setPersonInfo(person_id, response.data.data)
+      );
+
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
+};
+
+export const addPerson = (person_id = null, name = {}, next) => {
+  return async dispatch => {
+    try {
+
+      if (!person_id) {
+        throw String('Missing Person ID');
+      }
+
+      const token = await getCookie('token');
+  
+      if (!token) {
+        throw String('Missing Token');
+      }
+
+      dispatch(
+        setPersonInfo(person_id, name)
       );
 
       const response = await axios.post('http://10.0.0.50:5000/people/', {
         ...name,
+        person_id,
         token,
       });
 
