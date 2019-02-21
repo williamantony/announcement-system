3644f28be97d632ccca7f260605e6d69a1f42988
William Antony <william@willsquad.com>
2019-02-20 23:08:53 -0600

    Created ADD_PERSON_INFO, GET_PERSON_INFO & GET_PEOPLE_LIST reducers

--- src/redux/reducers/people.reducer.js
+++ src/redux/reducers/people.reducer.js
@@ -1,20 +1,40 @@
 import {
+  ADD_NEW_PERSON,
+  GET_PERSON_INFO,
+  GET_PEOPLE_LIST,
   PEOPLE_SET_INFO,
 } from '../actions';
 
 const initialState = {
   isSelected: false,
   selection: [],
-  entries: {
-
-  },
-  info: {
-
-  },
+  entries: {},
+  person: {},
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case ADD_NEW_PERSON:
+      return {
+        ...state,
+        person: action.payload,
+      };
+
+    case GET_PERSON_INFO:
+      return {
+        ...state,
+        person: {
+          ...state.person,
+          ...action.payload,
+        },
+      };
+
+    case GET_PEOPLE_LIST:
+      return {
+        ...state,
+        entries: action.payload,
+      };
+
     case PEOPLE_SET_INFO:
       return {
         ...state,
