72203dc132b807a4408067a39399816f967a3b9b
William Antony <william@willsquad.com>
2019-02-21 21:48:55 -0600

    Implemented setRowSelection() & toggleSelectAllRows() action_creators

--- src/redux/actions/table.action.js
+++ src/redux/actions/table.action.js
@@ -2,6 +2,8 @@
  * Table
  */
 export const CREATE_ROW = 'CREATE_ROW'; 
+export const SET_ROW_SELECTION = 'SET_ROW_SELECTION';
+export const TOGGLE_SELECT_ALL_ROWS = 'TOGGLE_SELECT_ALL_ROWS';
 export const CHECK_ROW_SELECTION = 'CHECK_ROW_SELECTION';
 
 export const createRow = (id = null) => {
@@ -23,6 +25,35 @@ export const checkRowSelection = () => {
     dispatch({
       type: CHECK_ROW_SELECTION,
     });
+
+  };
+};
+
+export const setRowSelection = (id = null, selection = false) => {
+  return dispatch => {
+    if (!id) return false;
+
+    dispatch({
+      type: SET_ROW_SELECTION,
+      payload: {
+        id,
+        selection,
+      },
+    });
+
+    dispatch(checkRowSelection());
+
+  };
+};
+
+export const toggleSelectAllRows = () => {
+  return dispatch => {
+    
+    dispatch({
+      type: TOGGLE_SELECT_ALL_ROWS,
+    });
+
+    dispatch(checkRowSelection());
 
   };
 };
