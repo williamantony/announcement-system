c8b0cf84e5e5d4cdd316e9b01ea98b70ba22752d
William Antony <william@willsquad.com>
2019-02-21 21:51:12 -0600

    Implemented toggleSelectAllRows() in TableHead using redux

--- src/components/Table/components/TableHead/TableHead.js
+++ src/components/Table/components/TableHead/TableHead.js
@@ -1,33 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { toggleSelectAllRows } from '../../../../redux/actions';
 import uuid from 'uuid/v4';
 import './TableHead.css';
 
-const TableHead = ({ columns = null }) => {
-  if (!Array.isArray(columns))
-    return null;
+class TableHead extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      columns: props.columns || [],
+      isAllSelected: false,
+    };
+  }
 
-  return (
-    <div className="TableHead">
-      <div className="TableHead__row">
-        <div className="TableHead__row__selector" data-selected="false">
-          <div className="TableHead__row__selector__icon" />
-        </div>
-        <div className="TableHead__row__content">
-          {
-            columns.map(column => (
-              <div
-                key={uuid()}
-                className="TableHead__row__content__item"
-                style={column.headStyle}
-              >
-                { column.name }
-              </div>
-            ))
-          }
+  static getDerivedStateFromProps(props, state) {
+    if (props.isAllSelected !== state.isAllSelected) {
+      return {
+        isAllSelected: props.isAllSelected,
+      };
+    }
+    return null;
+  }
+
+  toggleSelectAll = () => {
+    this.props.toggleSelectAllRows();
+  }
+
+  render() {
+    return (
+      <div className="TableHead">
+        <div className="TableHead__row">
+          <div
+            className="TableHead__row__selector"
+            data-selected={this.state.isAllSelected}
+            onClick={this.toggleSelectAll}
+          >
+            <div className="TableHead__row__selector__icon" />
+          </div>
+          <div className="TableHead__row__content">
+            {
+              this.state.columns.map(column => (
+                <div
+                  key={uuid()}
+                  className="TableHead__row__content__item"
+                  style={column.headStyle}
+                >
+                  { column.name }
+                </div>
+              ))
+            }
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    isAllSelected: state.table.isAllSelected,
+  };
 };
 
-export default TableHead;
+const mapDispatchToProps = {
+  toggleSelectAllRows,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(TableHead);
