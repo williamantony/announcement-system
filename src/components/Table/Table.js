abf357d3b9e2ef8d4b800b807ab358b1cf0e0e90
William Antony <william@willsquad.com>
2019-02-23 18:23:33 -0600

    Implemented createTable & updateTable with redux in Table Component

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { createTable, updateTable } from '../../redux/actions';
+import uuid from 'uuid/v4';
 import TableHead from './components/TableHead/TableHead';
 import TableBody from './components/TableBody/TableBody';
 import TableMenu from './components/TableMenu/TableMenu';
@@ -6,18 +9,39 @@ import TableMenu from './components/TableMenu/TableMenu';
 class Table extends Component {
   constructor(props) {
     super(props);
+    this.name = props.name || uuid();
     this.state = {
-      columns: props.columns || null,
-      rows: props.rows || null,
+      columns: props.columns || [],
+      rows: props.rows || [],
     };
   }
 
   static getDerivedStateFromProps(props, state) {
-    const { rows } = props;
-    if (JSON.stringify(rows) !== JSON.stringify(state.rows)) {
-      return { rows };
-    }
+    const { columns, rows, _columns, _rows } = props;
+
+    if (!!rows && !!columns && (
+      JSON.stringify(rows) !== JSON.stringify(state.rows) ||
+      JSON.stringify(columns) !== JSON.stringify(state.columns)
+    )) return { columns, rows };
+
+    if (!!_rows && !!_columns && (
+      JSON.stringify(_rows) !== JSON.stringify(state.rows) ||
+      JSON.stringify(_columns) !== JSON.stringify(state.columns)
+    )) return { columns, rows: _rows };
+
     return null;
+  }
+
+  componentDidMount() {
+    const tableConfig = {
+      columns: this.state.columns,
+      rows: this.state.rows,
+    };
+    this.props.createTable(this.name, tableConfig);
+  }
+
+  componentDidUpdate() {
+    this.props.updateTable(this.name, this.state);
   }
 
   render() {
@@ -28,7 +52,7 @@ class Table extends Component {
     const onMenu = this.props.onMenu || null;
     const customMenu = this.props.customMenu || null;
 
-    if (!Array.isArray(columns) && !Array.isArray(rows))
+    if (columns.length === 0 && rows.length === 0)
       return null;
 
     return (
@@ -40,10 +64,25 @@ class Table extends Component {
           customMenu={customMenu}
         />
         <TableHead columns={columns} />
-        <TableBody columns={columns} rows={rows} />
+        <TableBody columns={columns} rows={rows} tableName={this.name} />
       </div>
     );
   }
 }
 
-export default Table;
+const mapStateToProps = (state, ownProps) => {
+  const table = state.table.instances[ownProps.name];
+  if (!!table) {
+    return {
+      _columns: table.columns,
+      _rows: table.rows,
+    };
+  }
+  return {};
+};
+
+const mapDispatchToProps = {
+  createTable,updateTable
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Table);
