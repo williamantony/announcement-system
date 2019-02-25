54e80a787de692fbdf4074c47a439a71c28fc329
William Antony <william@willsquad.com>
2019-02-25 12:23:24 -0600

    Created createTable() and updateTable() action_creator

--- src/redux/actions/table.action.js
+++ src/redux/actions/table.action.js
@@ -1,85 +1,40 @@
 /**
  * Table
  */
-export const CREATE_ROW = 'CREATE_ROW';
-export const DELETE_ROWS = 'DELETE_ROWS';
-export const SET_ROW_SELECTION = 'SET_ROW_SELECTION';
-export const TOGGLE_SELECT_ALL_ROWS = 'TOGGLE_SELECT_ALL_ROWS';
-export const CHECK_ROW_SELECTION = 'CHECK_ROW_SELECTION';
-export const GET_SELECTED_ROWS = 'GET_SELECTED_ROWS';
+export const CREATE_TABLE = 'CREATE_TABLE';
+export const UPDATE_TABLE = 'UPDATE_TABLE';
 
-export const createRow = (id = null) => {
+export const createTable = (tableId = null, config = {}) => {
   return dispatch => {
-    if (!id) return false;
+
+    if (tableId === null)
+      return false;
 
     dispatch({
-      type: CREATE_ROW,
+      type: CREATE_TABLE,
       payload: {
-        id,
+        tableId,
+        columns: config.columns || [],
+        rows: config.rows || [],
       },
     });
 
-    dispatch(checkRowSelection());
-
   };
 };
 
-export const deleteRows = (rows = []) => {
+export const updateTable = (tableId = null, updateData = {}) => {
   return dispatch => {
 
+    if (tableId === null)
+      return false;
+
     dispatch({
-      type: DELETE_ROWS,
+      type: UPDATE_TABLE,
       payload: {
-        rows,
+        tableId,
+        updateData,
       },
     });
 
-    dispatch(checkRowSelection());
-
-  };
-};
-
-export const checkRowSelection = () => {
-  return dispatch => {
-
-    dispatch({
-      type: CHECK_ROW_SELECTION,
-    });
-
-  };
-};
-
-export const setRowSelection = (id = null, selection = false) => {
-  return dispatch => {
-    if (!id) return false;
-
-    dispatch({
-      type: SET_ROW_SELECTION,
-      payload: {
-        id,
-        selection,
-      },
-    });
-
-    dispatch(checkRowSelection());
-
-  };
-};
-
-export const toggleSelectAllRows = () => {
-  return dispatch => {
-    
-    dispatch({
-      type: TOGGLE_SELECT_ALL_ROWS,
-    });
-
-    dispatch(checkRowSelection());
-
-  };
-};
-
-export const getSelectedRows = () => {
-  return {
-    type: GET_SELECTED_ROWS,
   };
 };
