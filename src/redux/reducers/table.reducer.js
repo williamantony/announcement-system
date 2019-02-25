e32de9c52b4a8d07246f9f7d05132368d6acbfb4
William Antony <william@willsquad.com>
2019-02-25 12:21:45 -0600

    Created reducers for CREATE_TABLE and UPDATE_TABLE

--- src/redux/reducers/table.reducer.js
+++ src/redux/reducers/table.reducer.js
@@ -1,121 +1,39 @@
 import {
-  CREATE_ROW,
-  DELETE_ROWS,
-  SET_ROW_SELECTION,
-  TOGGLE_SELECT_ALL_ROWS,
-  CHECK_ROW_SELECTION,
-  GET_SELECTED_ROWS,
+  CREATE_TABLE,
+  UPDATE_TABLE,
 } from '../actions';
 
 const initialState = {
-  rows: {},
-  selection: [],
-  isSelected: false,
-  isMultiSelected: false,
-  isAllSelected: false,
+  instances: {
+
+  },
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case CREATE_ROW:
+    case CREATE_TABLE:
       return {
         ...state,
-        rows: {
-          ...state.rows,
-          [action.payload.id]: {
-            ...state[action.payload.id],
-            isSelected: false,
+        instances: {
+          ...state.instances,
+          [action.payload.tableId] : {
+            columns: action.payload.columns,
+            rows: action.payload.rows,
+            selection: [],
           },
         },
       };
 
-    case SET_ROW_SELECTION:
+    case UPDATE_TABLE:
       return {
         ...state,
-        rows: {
-          ...state.rows,
-          [action.payload.id]: {
-            ...state[action.payload.id],
-            isSelected: action.payload.selection,
+        instances: {
+          ...state.instances,
+          [action.payload.tableId] : {
+            ...state.instances[action.payload.tableId],
+            ...action.payload.updateData,
           },
         },
-      };
-    
-    case TOGGLE_SELECT_ALL_ROWS:
-      return {
-        ...state,
-        isAllSelected: !state.isAllSelected,
-        rows: Object.keys(state.rows).reduce((rows, row_id) => {
-          return {
-            ...rows,
-            [row_id]: {
-              ...state.rows[row_id],
-              isSelected: !state.isAllSelected,
-            },
-          };
-        }, {}),
-      };
-
-    case CHECK_ROW_SELECTION:
-      return {
-        ...state,
-        ...(() => {
-
-          const selection = {
-            isSelected: false,
-            isMultiSelected: false,
-          };
-
-          let isAllSelected = true;
-  
-          for (let i = 0; i < Object.keys(state.rows).length; i++) {
-  
-            const row_id = Object.keys(state.rows)[i];
-            const row = state.rows[row_id];
-  
-            if (row.isSelected) {
-              if (selection.isSelected) {
-                selection.isMultiSelected = true;
-                return selection;
-              }
-              selection.isSelected = true;
-            } else {
-              isAllSelected = false;
-            }
-  
-            if (i === Object.keys(state.rows).length - 1) {
-              return {
-                ...selection,
-                isAllSelected,
-              };
-            }
-  
-          }
-  
-        })(),
-      };
-
-    case GET_SELECTED_ROWS:
-      return {
-        ...state,
-        selection: Object.keys(state.rows).reduce((list, row_id) => {
-          if (state.rows[row_id].isSelected)
-            return [ ...list, row_id ];
-          return list;
-        }, []),
-      }
-    
-    case DELETE_ROWS:
-      return {
-        ...state,
-        rows: Object.keys(state.rows).reduce((rows, row_id) => {
-          const rowMatch = action.payload.rows.findIndex(id => row_id === id);
-          return !rowMatch
-            ? {
-              ...rows,
-              [row_id]: state.rows[row_id],
-            } : rows;
-        }, {}),
       };
 
     default:
