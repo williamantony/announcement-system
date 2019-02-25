30c00dc03bb4ba762f259c0e484c67b3da741f94
William Antony <william@willsquad.com>
2019-02-25 12:26:45 -0600

    Created toggleRowSelection() action_creator

--- src/redux/actions/table.action.js
+++ src/redux/actions/table.action.js
@@ -3,6 +3,7 @@
  */
 export const CREATE_TABLE = 'CREATE_TABLE';
 export const UPDATE_TABLE = 'UPDATE_TABLE';
+export const SET_TABLE_SELECTION = 'SET_TABLE_SELECTION';
 
 export const createTable = (tableId = null, config = {}) => {
   return dispatch => {
@@ -33,6 +34,29 @@ export const updateTable = (tableId = null, updateData = {}) => {
       payload: {
         tableId,
         updateData,
+      },
+    });
+
+  };
+};
+
+export const toggleRowSelection = (tableId = null, rowId) => {
+  return (dispatch, getState) => {
+
+    if (tableId === null)
+      return false;
+
+    let selection = getState().table.instances[tableId].selection;
+    const isSelected = selection.findIndex(item => item === rowId) !== -1;
+
+    selection = (isSelected) ? selection.filter(id => id !== rowId) : [ ...selection, rowId ];
+
+    dispatch({
+      type: SET_TABLE_SELECTION,
+      payload: {
+        tableId,
+        rowId,
+        selection,
       },
     });
 
